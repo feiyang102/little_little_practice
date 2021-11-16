961b33c5c5c4b31a8b6018c1d9265f4f9faa7b07
App1024 <279149270@qq.com>
2021-11-17 00:11:27 +0800

    vue-nav-menu-1 CSS美化部分完成，项目完成

--- vue-nav-menu-1/src/components/Home.vue
+++ vue-nav-menu-1/src/components/Home.vue
@@ -7,7 +7,7 @@
       <a href="#" class="CONTACT" @click="isActive('CONTACT')">CONTACT</a>
     </nav>
     <div>
-      <p>
+      <p class="content">
         你选择了<b>{{ active }}菜单</b>
       </p>
     </div>
@@ -30,8 +30,20 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  /* 为了去除 a 标签之间的空隙 */
+  font-size: 0px;
+}
 nav > a {
   background-color: #5597b4;
+  display: inline-block;
+  height: 100px;
+  padding: 0 20px;
+  text-decoration: none;
+  font-weight: bold;
+  line-height: 100px;
+  font-size: 36px;
+  color: white;
 }
 
 nav.HOME .HOME,
@@ -39,5 +51,17 @@ nav.PROJECTS .PROJECTS,
 nav.SERVICE .SERVICE,
 nav.CONTACT .CONTACT {
   background-color: #e35885;
+}
+
+p.content{
+  font-size: 36px;
+  color: #7d9098;
+}
+
+p.content>b{
+  color: white;
+  display: inline-block;
+  background-color: #c4d7e0;
+  padding:2px 12px
 }
 </style>
