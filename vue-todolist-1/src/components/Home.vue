89b2b36573e24b4c7ebf474c08d38e157670ba04
App1024 <279149270@qq.com>
2021-11-16 10:28:50 +0800

    实现了待办事项的操作逻辑：添加任务、删除任务

---
+++ vue-todolist-1/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="content">
+    <div class="content-1">
+      <div class="input">
+        <input
+          type="text"
+          placeholder="输入内容"
+          v-model="inputText"
+          @keypress.enter="addlist"
+        />
+      </div>
+      <div class="list-body">
+        <div
+          v-for="(item, index) in todolist"
+          v-bind:class="item.isDone ? 'done' : 'doing'"
+          v-bind:key="index"
+          @click="item.isDone = !item.isDone"
+        >
+          {{ item.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      todolist: [],
+      inputText: ''
+    }
+  },
+  methods: {
+    addlist: function () {
+      this.todolist.push({ text: this.inputText, isDone: false })
+      console.log(this.inputText)
+      this.inputText = ''
+    }
+  }
+}
+</script>
+
+<style>
+html,
+body,
+.content {
+  height: 100%;
+  overflow: hidden;
+}
+
+.content {
+  width: 80%;
+  background-color: darkgrey;
+}
+.doing {
+  color: red;
+  font-weight: bolder;
+}
+.done {
+  text-decoration-line: line-through;
+  color: black;
+  font-weight: 200;
+}
+</style>
