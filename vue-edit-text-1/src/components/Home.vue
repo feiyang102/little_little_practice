9b878aa241dcb3b7071a1358a56bffad8c81022a
App1024 <279149270@qq.com>
2021-11-19 00:42:24 +0800

    vue-eidt-text-1 实现核心交互逻辑

---
+++ vue-edit-text-1/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="content" @click="toggleEditInput">
+    <div class="editinput" v-show="isShow">
+      <input type="text" name="" id="" v-model="val" @click.stop="showEditInput" />
+    </div>
+    <div class="edittext" @click.stop="toggleEditInput">{{ val }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      isShow: false,
+      val: '点我，并编辑内容。'
+    }
+  },
+  methods: {
+    toggleEditInput: function () {
+      this.isShow = !this.isShow
+    },
+    showEditInput: function () {
+      this.isShow = true
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+  width: 300px;
+}
+.editinput {
+  width: 250px;
+}
+.edittext {
+  font-size: 36px;
+  color: #9c9891;
+  font-weight: 700;
+}
+.edittext::before {
+  content: "✎";
+  margin-right: 5px;
+}
+</style>
